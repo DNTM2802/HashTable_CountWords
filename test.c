7be91ae9d0ca6f838c86a7472f46df9489fb5969
barroqueiro <cruzdinis@ua.pt>
2019-12-16 10:57:13 +0000

    Hash_Table (quase)

--- test.c
+++ test.c
@@ -15,6 +15,7 @@ file_data_t;
 //Representa cada palavra distinta num determinado ficheiro
 typedef struct word{
     struct word* next;
+    char word[64];
     int first_location;
     int last_location;
     int max_dist;
@@ -24,14 +25,14 @@ typedef struct word{
 }
 word_t;
 
+
 typedef struct hash_table{
     unsigned int size;
     unsigned int count;
-    word_t *table;
+    word_t *table[2000];
 }
 hash_table_t;
-
-typedef struct hash_node_t 
+ 
 
 int open_text_file(char *file_name,file_data_t *fd){
     fd->fp = fopen(file_name,"rb");
@@ -90,11 +91,49 @@ hash(unsigned char *str)
 }
 
 void main(){
-    word_t hashhtable[100];
     file_data_t *fl;
-
+    hash_table_t *hash_table;
+    int hashcode;
+    word_t word;
+    word_t *head;
+    bool is_in_hash;
     if(open_text_file("Teste.txt",fl)==-1){
         return EXIT_FAILURE;
+    }
+
+    while(read_word(fl) != -1){
+        hashcode=hash(fl->word) % hash_table->size;
+        head=hash_table->table[hashcode];
+        while(*head->next != NULL){
+            if (*head->next->word == fl->word){
+                is_in_hash=true;
+                int temp=*head->next->last_location;
+                int dist=fl->current_pos-temp;
+                *head->next->last_location=fl->current_pos;
+                *head->next->count++;
+                if (dist>*head->next->max_dist){
+                    *head->next->max_dist=dist;
+                }else if(dist<*head->next->max_dist){
+                    *head->next->min_dist=dist;
+                }else{
+
+                }
+                *head->next->medium_dist=*head->next->medium_dist+(dist-*head->next->medium)/ *head->next->count++;
+            }
+            head=head->next;
+        }
+        if (!is_in_hash){
+            word_t new;
+            head->next = &new;
+            new->next=NULL;
+            new->first_location=fl->current_pos;
+            new->last_location=fl->current_pos;
+            new->max_dist=0;
+            new->min_dist=0;
+            new->medium_dist=0;
+            new->count=1;
+        }
+        
     }
 
     
